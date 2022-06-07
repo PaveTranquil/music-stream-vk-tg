a3fd11dbc1c252c1ffe3c96fc2a05ca1cacdbcb4
Pavel Ovchinnikov <opavel@internet.ru>
2022-06-07 14:59:09 +0000

    init: переписан setup.py
    - Окно расширяется до 80 символов в ширину
    - Название и автор + форк
    - Инструкция для получения адреса сообщества
    - Инструкция для получения api_id и api_hash для Telegram
    - Проверка на длину статуса по умолчанию

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
-print('by Aradionov366')
-print('====Telegram====')
-print('api id и api hash получать на my.telegram.org')
-f = open('bd.py', 'w')
-api_id = input('Введите api id: ') #my.telegram.org
-api_hash = input('Введите api hash: ') #my.telegram.org
-f.write(f'api_id = {api_id}\napi_hash = "{api_hash}"\n')
-print('====Spotify====')
-print('client id и client secret получать на https://developer.spotify.com/dashboard/')
-client_id = input('Введите client id: ')
-client_secret = input('Введите client secret: ')
-redirect_uri = "http://localhost:8888/callback" #Не трогать (!)
-spotiusername = input('Введите юзернейм в spotify: ')
-f.write(f'client_id = "{client_id}"\nclient_secret = "{client_secret}"\nspotiusername = "{spotiusername}"\n')
-print('====Дополнительные функции====')
-status = input('Введите статус, который будет поставлен при условии, что spotify не работает: ')
-f.write(f'status = "{status}"')
-f.close()
-print(f'хорошо, скрипт настроен, авторизируйте spotify через эту ссылку:\nhttps://accounts.spotify.com/authorize?response_type=code&client_id={client_id}&scope=user-read-playback-state-user-libary-read&redirect_uri=http://localhost:8888/callback')
+import os
+
+os.system('mode con: cols=80')
+
+print('Трансляция музыки из ВКонтакте в статус Telegram'.center(80))
+print('by PaveTranquil (forked from Aradionov366)'.center(80))
+print()
+print('===== ВКонтакте ====='.center(80))
+print('1. Создайте закрытое сообщество — https://vk.com/groups_create')
+print('2. Выберите «Группа по интересам» и тип группы «Закрытая»')
+print('3. Скопируйте адрес группы и вставьте ниже (например, https://vk.com/club12345)')
+print('4. Откройте раздел «Музыка» — https://vk.com/audio')
+print('5. В плеере нажмите «Трансляция аудиозаписей» (третья кнопка с конца)')
+print('6. Выберите только что созданное сообщество\n')
+
+link = input('Вставьте адрес сообщества: ')
+
+print('===== Telegram ====='.center(80))
+print('1. Перейдите на https://my.telegram.org')
+print('2. Залогиньтесь и создайте приложение (нажмите «API development tools»)')
+print('3. Скопируйте api_id и api_hash\n')
+
+api_id = input('Введите api_id: ')
+api_hash = input('Введите api_hash: ')
+
+status = input('\n\nВведите статус, который будет стоять, если что-то сломается: ')
+while len(status) > 70:
+    status = input('Введите статус, который будет стоять, если что-то сломается: ')
+with open('bd.py', 'w', encoding='utf-8') as file:
+    file.write(f'api_id = {api_id}\napi_hash = "{api_hash}"\nstatus = "{status}"\ngroup_link = "{link}"\n')
