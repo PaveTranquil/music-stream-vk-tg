be5278c7be8bc870f425dd1d0258d88a9fb4a517
Pavel Ovchinnikov <opavel@internet.ru>
2022-06-08 05:57:37 +0000

    feat: дополнение в bd.py
    - Добавлены типы и подсказки к переменным, которым после выполнения setup.py будет присвоено значение

--- bd.py
+++ bd.py
@@ -1,1 +1,4 @@
-
+api_id: int  # Получать в my.telegram.org
+api_hash: str  # Получать в my.telegram.org
+status: str  # Статус по умолчанию
+group_link: str  # Ссылка на закрытое сообщество ВКонтакте
